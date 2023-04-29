fbee8096c73a04946dd4da3706d1297d54d60671
Krasilnikov Roman <x0kill@ya.ru>
2023-04-29 14:15:35 +0300

    Refactoring of registry module

--- src/shared/registry.ts
+++ src/shared/registry.ts
@@ -2,10 +2,10 @@
 export interface Config {}
 
 // @ts-expect-error Config is empty
-export const c: Config = {}
+const config: Config = {}
 
-export function defineConfig(config: Config): void {
-  Object.assign(c, config)
+export function defineConfig(cfg: Config): void {
+  Object.assign(config, cfg)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -31,6 +31,6 @@ export function defineService<K extends keyof Registry, V extends Registry[K]>(
   factory: Factory<V>
 ): void {
   Object.defineProperty(r, key, {
-    get: factory.bind(r, c),
+    get: factory.bind(r, config),
   })
 }

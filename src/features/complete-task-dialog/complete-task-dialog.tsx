7597fca2aef296914330b3fb4d440b4ead1fc105
Krasilnikov Roman <x0kill@ya.ru>
2023-05-03 02:58:51 +0300

    Fix dialog shifting

--- src/features/complete-task-dialog/complete-task-dialog.tsx
+++ src/features/complete-task-dialog/complete-task-dialog.tsx
@@ -58,6 +58,14 @@ export function CompleteTaskDialog(): JSX.Element {
       }}
       slotProps={{
         root: {
+          style: {
+            position: 'fixed',
+            zIndex: 1300,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            left: 0,
+          },
           onSubmit: handleSubmit(onSubmit),
         },
       }}

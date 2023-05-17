2081c137146078f53efa05ce5e3113cc53038e68
Krasilnikov Roman <x0kill@ya.ru>
2023-05-18 00:03:47 +0300

    Enable effector swc plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-// import react from '@vitejs/plugin-react-swc'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+// import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -7,12 +7,19 @@ export default defineConfig({
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
-      babel: {
-        babelrc: true,
-        plugins: [['effector/babel-plugin', { addLoc: true }]],
-        presets: ['atomic-router/babel-preset'],
-      },
-      // plugins: [['@effector/swc-plugin', {}]],
+      // babel: {
+      //   babelrc: true,
+      //   plugins: [['effector/babel-plugin', { addLoc: true }]],
+      //   presets: ['atomic-router/babel-preset'],
+      // },
+      plugins: [
+        [
+          '@effector/swc-plugin',
+          {
+            factories: ['atomic-router'],
+          },
+        ],
+      ],
     }),
   ],
   resolve: {
